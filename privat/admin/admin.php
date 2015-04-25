cb0d112c2e7fcec6ddf29e8e1055a700d1a6d007
radu.baloiu <radu_baloiu@yahoo.com>
2015-04-25 15:17:04 +0300

    nimic

--- privat/admin/admin.php
+++ privat/admin/admin.php
@@ -20,7 +20,7 @@
 ?>
 <html>
     <head>
-        <meta charset="UTF-8">
+        <meta charset= "UTF-8">
         <title></title>
         <link rel="stylesheet" type="text/css" href="../../style/bootstrap/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="../../style/bootstrap/bootstrap-theme.css">
